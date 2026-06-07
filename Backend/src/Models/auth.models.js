871246509825ed4d9edff25fe07eb95d7696cec8
Vaaiv <vaibhavyadav172004@gmail.com>
2026-06-07 17:45:49 -0400

    added the bcrypt package and used hashing concept to make password hashed and protected used functions of bcrypt to implement all this

--- Backend/src/Models/auth.models.js
+++ Backend/src/Models/auth.models.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+const bcrypt=require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -23,14 +25,26 @@ const userSchema = new mongoose.Schema({
     minlenght: [6, "password should contain more than 6 characters"],
     select: false
   },
-
-
-
 },
 {
   timestamps:true,
 });
 
-userSchema.pre("save", async function(next){
-  
+userSchema.pre("save", async function(next){      //these are some middlewares 
+  if(!this.isModified("password")){
+    return next();
+  }
+
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password=hash;
+
+  next();
 })
+
+userSchema.methods.comparepassword= async function(password){     //this will compare if the password is false or true
+  return await bcrypt.password(password, this.password);
+} 
+
+const userModel=mongoose.model("user", userSchema);
+
+module.exports=userModel;
